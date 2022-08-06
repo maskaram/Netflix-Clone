4a701ed1d782cba26b1d2cb1b5624b688f6f02da
maskaram <meskerem185@gmail.com>
2022-08-05 23:13:12 -0400

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,43 @@
+import React,{useState,useEffect} from 'react'
+import './Banner.css'
+import axios from './axios';
+import requests from './requests';
+function Banner() {
+    
+    const [movie, setmovie]=useState([])
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            console.log(request)
+            setmovie(request?.data.results[
+                Math.floor(Math.random() * request.data.results.length)
+            ]
+            );
+            return request;
+        }
+        
+        fetchData()},[])
+        function truncate(str, n) {
+            return str?.length > n ? str.substr(0, n - 1) + "..." : str;}
+
+  return (
+    <header
+    className="banner"
+  style={{
+    backgroundSize: "cover",
+    backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+    backgroundPosition: 'top center',}}>
+    <div className= 'banner__contents'>
+        <div className="banner__title">
+    {movie?.title || movie?.name || movie.original_name}
+    </div>
+    <div className="banner__description">{truncate
+    
+    (movie?.overview, 150 )}</div>
+</div>
+  <div className="banner__fadeBottom"/>
+   </header>
+  )
+}
+
+export default Banner
---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = `b4e3b46a09317e71217628ede954b5f7`;
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_Networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/movie/comedy?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests
+//https://api.themoviedb.org/3/trending/all/week?api_key=b4e3b46a09317e71217628ede954b5f7&language=en-US//
